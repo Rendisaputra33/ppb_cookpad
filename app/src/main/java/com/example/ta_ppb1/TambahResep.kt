b8215f7ed54453785cb36489aef065aa4d02e729
Manan <113414988+Khudorimanan@users.noreply.github.com>
2023-12-19 15:31:25 +0700

    Create TambahResep.kt

---
+++ app/src/main/java/com/example/ta_ppb1/TambahResep.kt
@@ -0,0 +1,11 @@
+package com.example.ta_ppb1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRecipe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_resep)
+    }
+}

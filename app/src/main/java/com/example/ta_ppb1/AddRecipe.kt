e13c43ba51bcfe40101af3ba66bed90292a1e091
ShierlyMargaretha <eytetelepta@gmail.com>
2023-12-25 18:49:53 +0700

    ganti front end

--- app/src/main/java/com/example/ta_ppb1/AddRecipe.kt
+++ app/src/main/java/com/example/ta_ppb1/AddRecipe.kt
@@ -2,10 +2,21 @@ package com.example.ta_ppb1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ta_ppb1.databinding.ActivityMainBinding
+import com.example.ta_ppb1.databinding.ActivityTambahresepBinding
+import com.example.ta_ppb1.room.RoomDatabase
 
 class AddRecipe : AppCompatActivity() {
+    private lateinit var binding: ActivityTambahresepBinding
+
+    val database by lazy{RoomDatabase(this)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityTambahresepBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_tambahresep)
+
+        binding.unggah.setOnClickListener {
+
+        }
     }
 }

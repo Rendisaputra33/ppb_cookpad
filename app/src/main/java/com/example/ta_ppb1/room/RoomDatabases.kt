d7e4b6ca5f417bebaf950b683ab6068bb172ceaf
Rendisaputra33 <gorengmbah@gmail.com>
2023-12-25 16:55:59 +0700

    update mael

--- app/src/main/java/com/example/ta_ppb1/room/RoomDatabases.kt
+++ app/src/main/java/com/example/ta_ppb1/room/RoomDatabases.kt
@@ -10,14 +10,14 @@ import com.example.ta_ppb1.repository.RecipeRepository
 import com.example.ta_ppb1.repository.UserRepository
 
 @Database(entities = [User::class, Recipe::class], version = 1)
-abstract class RoomDatabase: RoomDatabase() {
+abstract class RoomDatabases: RoomDatabase() {
     abstract fun recipeRepository(): RecipeRepository
     abstract fun userRepository(): UserRepository
 
     companion object {
         private val LOCK = Any()
 
-        private var instace: RoomDatabase? = null
+        private var instace: RoomDatabases? = null
 
         operator fun invoke(ctx: Context) = instace ?: synchronized(LOCK) {
             instace ?: init(ctx).also {
@@ -27,7 +27,7 @@ abstract class RoomDatabase: RoomDatabase() {
 
         private fun init(ctx: Context) = Room.databaseBuilder(
             ctx.applicationContext,
-            RoomDatabase::class.java,
+            RoomDatabases::class.java,
             "recipe-db.db"
         ).build()
     }

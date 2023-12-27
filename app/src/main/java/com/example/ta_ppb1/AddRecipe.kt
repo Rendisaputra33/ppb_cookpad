b9eab282de64cea3593ef247234ebeb588e6e167
ShierlyMargaretha <eytetelepta@gmail.com>
2023-12-27 21:49:07 +0700

    BE tambah resep

--- app/src/main/java/com/example/ta_ppb1/AddRecipe.kt
+++ app/src/main/java/com/example/ta_ppb1/AddRecipe.kt
@@ -1,22 +1,34 @@
 package com.example.ta_ppb1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ta_ppb1.databinding.ActivityMainBinding
 import com.example.ta_ppb1.databinding.ActivityTambahresepBinding
-import com.example.ta_ppb1.room.RoomDatabase
+import com.example.ta_ppb1.entity.Recipe
+import com.example.ta_ppb1.room.RoomDatabases
 
 class AddRecipe : AppCompatActivity() {
     private lateinit var binding: ActivityTambahresepBinding
 
-    val database by lazy{RoomDatabase(this)}
+    private val database by lazy {RoomDatabases(this)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTambahresepBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_tambahresep)
+        setContentView(binding.root)
 
         binding.unggah.setOnClickListener {
+        val foto = binding.editFoto.text.toString()
 
+        val nama = binding.editNama.text.toString()
+
+        val bahan = binding.editBahan.text.toString()
+
+        val cara = binding.editCara.text.toString()
+            val recipe =  Recipe (1, 1, nama,"", bahan, cara, foto)
+        database.recipeRepository().insertAll(recipe)
+    val itn = Intent (this,EditResep::class.java)
+            startActivity(itn)
         }
     }
 }

c3940939c511a4f02fcc145dc19c97a573f2924f
Rendi Saputra <68989984+Rendisaputra33@users.noreply.github.com>
2024-02-05 13:41:06 +0700

    Update User.kt

--- app/src/main/java/com/example/ta_ppb1/entity/User.kt
+++ app/src/main/java/com/example/ta_ppb1/entity/User.kt
@@ -2,8 +2,9 @@ package com.example.ta_ppb1.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Index
 
-@Entity(tableName = "users")
+@Entity(tableName = "users", indices = [Index(value = ["email"], unique = true)])
 data class User(
     val email: String,
     val password: String,
